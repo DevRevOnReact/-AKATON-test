cf6ed3841ae8b136fe17a12940114bfb9d5826bd
DanilRev04 <danilrev04@gmail.com>
2022-12-06 02:46:43 +0300

    Create Quiz component Commit

--- xakaton/src/App.js
+++ xakaton/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import HomePage from "./Components/HomePage.tsx";
 import DescriptionPage from "./Components/DescriptionPage.tsx";
 import Session from "./Components/Session.tsx";
+import Quiz from "./Components/Quiz.tsx";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      {/*<HomePage />*/}
    {/* <DescriptionPage/>*/}
     {/*  <Session />*/}
+        <Quiz />
     </div>
   );
 }
